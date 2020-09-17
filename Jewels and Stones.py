91c5594d2c0371710feb0b290b26c092d605f97c
hsinhuibiga <hsinhuibiga@gmail.com>
2020-09-17 13:59:14 +0800

    Add files via upload

---
+++ Jewels and Stones.py
@@ -0,0 +1,10 @@
+#Jewels and Stones
+
+class Solution(object):
+    def numJewelsInStones(self, J, S):
+        """
+        :type J: str
+        :type S: str
+        :rtype: int
+        """
+        return sum(S.count(j) for j in J)
