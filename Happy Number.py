bad5d14bcc999b49166e37a50a9c0563db5ebb53
hsinhuibiga <hsinhuibiga@gmail.com>
2020-09-17 12:00:56 +0800

    Add files via upload

---
+++ Happy Number.py
@@ -0,0 +1,18 @@
+#Happy Number
+
+class Solution(object):
+    def isHappy(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        got = set()
+        while n != 1 and n not in got:
+            got.add(n)
+            sum = 0
+            while n:
+                sum += (n % 10)**2
+                n //= 10
+            n = sum
+
+        return n == 1
